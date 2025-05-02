7bce4ddd4ead9c9028dd7e50ec5510b57d3731c8
0xRuangsak <0xruangsak@gmail.com>
2025-05-02 12:15:17 +0700

    export { StorageProvider } from './interfaces/storage-provider.interface'; export { RedisConfig } from './interfaces/redis-config.interface'; export { RedisStorageProvider } from './providers/redis-storage.provider';

---
+++ packages/storage/src/index.ts
@@ -0,0 +1,3 @@
+export { StorageProvider } from "./interfaces/storage-provider.interface";
+export { RedisConfig } from "./interfaces/redis-config.interface";
+export { RedisStorageProvider } from "./providers/redis-storage.provider";
